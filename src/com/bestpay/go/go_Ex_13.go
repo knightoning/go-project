8638b8ccfe432bc81c7fed18679c7a6a9f6b1795
hujie <hujie-it@bestpay.com.cn>
2018-03-26 10:38:30 +0800

    add zhushi

--- src/com/bestpay/go/go_Ex_13.go
+++ src/com/bestpay/go/go_Ex_13.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+/**
+结构体
+ */
+
+ /**
+ 这里的 person 结构体包含了 name 和 age 两个字段。
+  */
 type person struct{
 	name string
 	age int
@@ -15,11 +22,17 @@ func main(){
 
 	fmt.Println(person{name:"Fred"})
 
+	/**
+	& 前缀生成一个结构体指针。
+	 */
 	fmt.Println(&person{name:"Ann",age:40})
 
 	s := person{name:"Sean",age:50}
 	fmt.Println(s.name)
 
+	/**
+	也可以对结构体指针使用. - 指针会被自动解引用。
+	 */
 	sp := &s
 	fmt.Println(sp.age)
 
