ec021cf61fe6de376d7e50222d78fd991829838d
hujie <hujie20081122@163.com>
2019-06-17 14:43:31 +0800

    add new

---
+++ src/com/bestpay/go/go_Ex_90.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	array := [3] float64{7.0,8.5,9.1}
+
+	x := Sum(&array)
+
+	fmt.Printf("The sum of the array is: %f",x)
+
+}
+
+func Sum(a *[3]float64)(sum float64)  {
+
+	for _,V := range a{
+		sum += V
+	}
+	return
+}
