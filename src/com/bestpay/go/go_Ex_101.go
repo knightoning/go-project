016906b795ec422b74fa3c3b0598b64a6db20cb7
hujie <hujie20081122@163.com>
2019-07-16 19:15:57 +0800

    add new go

---
+++ src/com/bestpay/go/go_Ex_101.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type struct1 struct {
+
+	i1  int
+	f1  float32
+	str string
+}
+
+func main()  {
+
+	ms := new (struct1)
+	ms.i1 = 10
+	ms.f1 = 15.5
+	ms.str = "Chris"
+
+	fmt.Printf("The int is: %d\n",ms.i1)
+	fmt.Printf("float int is: %f\n",ms.f1)
+	fmt.Printf("float string is: %s\n",ms.str)
+	fmt.Println(ms)
+}
