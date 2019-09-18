4961545df1b312cf84011c10add88e9fca1a2626
hujie <hujie20081122@163.com>
2019-09-18 09:17:06 +0800

    add veresion

---
+++ src/com/bestpay/go/go_Ex_150.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	)
+
+func main()  {
+
+	suck(pump())
+	time.Sleep(1e9)
+}
+
+func suck(ch chan int)  {
+
+	go func() {
+		for v:= range ch{
+			fmt.Println(v)
+		}
+	}()
+}
+
+func pump()  chan int{
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; ; i++{
+			ch <- i
+		}
+	}()
+
+	return ch
+}
