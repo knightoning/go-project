b7e2976078e0928b267c949bbed07e2bf1edaa6f
hujie <hujie20081122@163.com>
2019-10-10 14:18:42 +0800

    add

---
+++ src/com/bestpay/go/go_Ex_155.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+import "time"
+
+func main()  {
+
+	tick := time.Tick(1e8)
+	boom := time.After(5e8)
+
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.！")
+
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("     .")
+			time.Sleep(5e7)
+		}
+
+	}
+}
